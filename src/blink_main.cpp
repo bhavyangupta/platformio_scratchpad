9baefde2ec445d3c347f3df5092c6f464a16b8cc
Bhavya Gupta <bhavyangupta@gmail.com>
2016-07-28 15:56:41 -0500

    First commit

---
+++ src/blink_main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+
+}
